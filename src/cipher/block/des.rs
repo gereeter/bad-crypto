0ba1b4855ed6059e044566b79cade59b8c938a6f
Jonathan S <gereeter+code@gmail.com>
2015-10-24 16:50:08 -0500

    Move tables for DES into a submodule

--- src/cipher/block/des.rs
+++ src/cipher/block/des.rs
@@ -54,56 +54,19 @@ fn run_permutation(perm: &[u8], val: Secret<u64>) -> Secret<u64> {
     out
 }
 
-// See FIPS Publication 46-3, Appendix 1
-const INITIAL_PERMUTATION: [u8; 64] = [
-    58, 50, 42, 34, 26, 18, 10, 2,
-    60, 52, 44, 36, 28, 20, 12, 4,
-    62, 54, 46, 38, 30, 22, 14, 6,
-    64, 56, 48, 40, 32, 24, 16, 8,
-    57, 49, 41, 33, 25, 17, 9,  1,
-    59, 51, 43, 35, 27, 19, 11, 3,
-    61, 53, 45, 37, 29, 21, 13, 5,
-    63, 55, 47, 39, 31, 23, 15, 7
-];
-
 // Inverse of final_permute
 fn initial_permute(block: Secret<u64>) -> Secret<u64> {
-    run_permutation(&INITIAL_PERMUTATION, block)
+    run_permutation(&tables::INITIAL_PERMUTATION, block)
 }
-
-// See FIPS Publication 46-3, Appendix 1
-const FINAL_PERMUTATION: [u8; 64] = [
-    40, 8, 48, 16, 56, 24, 64, 32,
-    39, 7, 47, 15, 55, 23, 63, 31,
-    38, 6, 46, 14, 54, 22, 62, 30,
-	37, 5, 45, 13, 53, 21, 61, 29,
-	36, 4, 44, 12, 52, 20, 60, 28,
-	35, 3, 43, 11, 51, 19, 59, 27,
-	34, 2, 42, 10, 50, 18, 58, 26,
-	33, 1, 41, 9,  49, 17, 57, 25
-];
 
 // Inverse of initial_permute
 fn final_permute(block: Secret<u64>) -> Secret<u64> {
-    run_permutation(&FINAL_PERMUTATION, block)
+    run_permutation(&tables::FINAL_PERMUTATION, block)
 }
-
-
-// See FIPS Publication 46-3, Appendix 1
-const EXPANSION_PERMUTATION: [u8; 48] = [
-	32, 1,  2,  3,  4,  5,
-	4,  5,  6,  7,  8,  9,
-	8,  9,  10, 11, 12, 13,
-	12, 13, 14, 15, 16, 17,
-	16, 17, 18, 19, 20, 21,
-	20, 21, 22, 23, 24, 25,
-	24, 25, 26, 27, 28, 29,
-	28, 29, 30, 31, 32, 1
-];
 
 // Returns 48 bits
 fn expand(half_block: Secret<u32>) -> Secret<u64> {
-    run_permutation(&EXPANSION_PERMUTATION, Secret::<u64>::from(half_block))
+    run_permutation(&tables::EXPANSION_PERMUTATION, Secret::<u64>::from(half_block))
 }
 
 // Takes 48 bits
@@ -111,17 +74,51 @@ fn substitute(block: Secret<u64>) -> Secret<u32> {
     unimplemented!()
 }
 
+fn permute(block: Secret<u32>) -> Secret<u32> {
+    run_permutation(&tables::ROUND_PERMUTATION, Secret::<u64>::from(block)).truncate()
+}
 
 // See FIPS Publication 46-3, Appendix 1
-const ROUND_PERMUTATION: [u8; 32] = [
-	16, 7,  20, 21, 29, 12, 28, 17,
-	1,  15, 23, 26, 5,  18, 31, 10,
-	2,  8,  24, 14, 32, 27, 3,  9,
-	19, 13, 30, 6,  22, 11, 4,  25
-];
+mod tables {
+    pub const INITIAL_PERMUTATION: [u8; 64] = [
+	    58, 50, 42, 34, 26, 18, 10, 2,
+	    60, 52, 44, 36, 28, 20, 12, 4,
+	    62, 54, 46, 38, 30, 22, 14, 6,
+	    64, 56, 48, 40, 32, 24, 16, 8,
+	    57, 49, 41, 33, 25, 17, 9,  1,
+	    59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+    ];
 
-fn permute(block: Secret<u32>) -> Secret<u32> {
-    run_permutation(&ROUND_PERMUTATION, Secret::<u64>::from(block)).truncate()
+    pub const FINAL_PERMUTATION: [u8; 64] = [
+       40, 8, 48, 16, 56, 24, 64, 32,
+       39, 7, 47, 15, 55, 23, 63, 31,
+       38, 6, 46, 14, 54, 22, 62, 30,
+       37, 5, 45, 13, 53, 21, 61, 29,
+       36, 4, 44, 12, 52, 20, 60, 28,
+       35, 3, 43, 11, 51, 19, 59, 27,
+       34, 2, 42, 10, 50, 18, 58, 26,
+       33, 1, 41, 9,  49, 17, 57, 25
+    ];
+
+    pub const EXPANSION_PERMUTATION: [u8; 48] = [
+        32, 1,  2,  3,  4,  5,
+        4,  5,  6,  7,  8,  9,
+        8,  9,  10, 11, 12, 13,
+        12, 13, 14, 15, 16, 17,
+        16, 17, 18, 19, 20, 21,
+        20, 21, 22, 23, 24, 25,
+        24, 25, 26, 27, 28, 29,
+        28, 29, 30, 31, 32, 1
+    ];
+
+    pub const ROUND_PERMUTATION: [u8; 32] = [
+        16, 7,  20, 21, 29, 12, 28, 17,
+        1,  15, 23, 26, 5,  18, 31, 10,
+        2,  8,  24, 14, 32, 27, 3,  9,
+        19, 13, 30, 6,  22, 11, 4,  25
+    ];
 }
 
 #[cfg(test)]
